a7431451592a13c7067e8cf3af814f30fcf2b824
Sigma711 <1979934715@qq.com>
2022-04-15 21:46:02 +0000

    Develop util

--- src/util/reporter.rs
+++ src/util/reporter.rs
@@ -5,36 +5,36 @@ use std::rc::Rc;
 
 #[derive(Clone)]
 pub struct LogReporter {
-  inner: Rc<RefCell<LogReporterInner>>,
+    inner: Rc<RefCell<LogReporterInner>>,
 }
 
 struct LogReporterInner {
-  ok: bool,
-  reason: String,
+    ok: bool,
+    reason: String,
 }
 
 impl LogReporter {
-  pub fn new() -> Self {
-    Self {
-      inner: Rc::new(RefCell::new(LogReporterInner {
-        ok: true,
-        reason: "".to_owned(),
-      })),
+    pub fn new() -> Self {
+        Self {
+            inner: Rc::new(RefCell::new(LogReporterInner {
+                ok: true,
+                reason: "".to_owned(),
+            })),
+        }
     }
-  }
-  pub fn result(&self) -> Result<()> {
-    let inner = self.inner.borrow();
-    if inner.ok {
-      Ok(())
-    } else {
-      Err(Error::Corruption(inner.reason.clone()))
+    pub fn result(&self) -> Result<()> {
+        let inner = self.inner.borrow();
+        if inner.ok {
+            Ok(())
+        } else {
+            Err(Error::Corruption(inner.reason.clone()))
+        }
     }
-  }
 }
 
 impl Reporter for LogReporter {
-  fn corruption(&mut self, _bytes: u64, reason: &str) {
-    self.inner.borrow_mut().ok = false;
-    self.inner.borrow_mut().reason = reason.to_owned();
-  }
+    fn corruption(&mut self, _bytes: u64, reason: &str) {
+        self.inner.borrow_mut().ok = false;
+        self.inner.borrow_mut().reason = reason.to_owned();
+    }
 }
