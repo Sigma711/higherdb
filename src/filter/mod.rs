ef99bbc18c55fd87fa0244731550df79fe69c930
Sigma711 <1979934715@qq.com>
2022-04-15 21:45:00 +0000

    Develop filter

--- src/filter/mod.rs
+++ src/filter/mod.rs
@@ -7,14 +7,14 @@ pub mod bloom;
 /// of keys or any other parameters, will be encoded in the byte filter returned by
 /// `new_filter_writer`.
 pub trait FilterPolicy: Send + Sync {
-  fn name(&self) -> &str;
+    fn name(&self) -> &str;
 
-  /// `may_contain` returns whether the encoded filter may contain given key.
-  /// False positives are possible, where it returns true for keys not in the
-  /// original set.
-  fn may_contain(&self, filter: &[u8], key: &[u8]) -> bool;
+    /// `may_contain` returns whether the encoded filter may contain given key.
+    /// False positives are possible, where it returns true for keys not in the
+    /// original set.
+    fn may_contain(&self, filter: &[u8], key: &[u8]) -> bool;
 
-  /// Creates a filter based on given keys
-  // TODO: use another type instead of &[Vec<u8>]
-  fn create_filter(&self, keys: &[Vec<u8>]) -> Vec<u8>;
+    /// Creates a filter based on given keys
+    // TODO: use another type instead of &[Vec<u8>]
+    fn create_filter(&self, keys: &[Vec<u8>]) -> Vec<u8>;
 }
